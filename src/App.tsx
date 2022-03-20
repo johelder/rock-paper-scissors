f0b88d7a5784b5f47fc8e414defa439d94468b97
johelder <johelder.araujo@pede.ai>
2022-03-20 16:27:57 -0300

    :tada: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Game from './pages/Game';
+
+function App() {
+  return (
+    <div className="App">
+      <Game title="alex" />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+interface IButtonProps {
+  type: string;
+}
+
+export function Button({ type }: IButtonProps) {
+  return (
+    <button>
+      teste
+    </button>
+  );
+}
