417d032704bf1e2b1a9164e45145f5aa9c67f012
johelder <johelder.arj@gmail.com>
2022-04-21 17:06:00 -0300

    feat: importing Game page at App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import Game from './pages/Game';
+import Game from "./pages/Game";
 
 function App() {
-  return (
-    <div className="App">
-      <Game title="alex" />
-    </div>
-  )
+  return <Game />;
 }
 
 export default App;
