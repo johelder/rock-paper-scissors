b6b7725f6f95c5b9c4b17bed96d44c7bd8b00cc8
johelder <johelder.arj@gmail.com>
2022-05-24 22:26:24 -0300

    test: adding display header score test

--- src/components/DisplayHeader/DisplayHeader.spec.tsx
+++ src/components/DisplayHeader/DisplayHeader.spec.tsx
@@ -5,9 +5,15 @@ import { DisplayHeader } from ".";
 describe("Display Header Component", () => {
   it("should render rock-paper-scissors logo", () => {
     const { getByTestId } = render(<DisplayHeader scoreCount={1} />);
-
     const appLogo = getByTestId("App Logo");
 
     expect(appLogo).toBeInTheDocument();
+  });
+
+  it("should display score count correctly", () => {
+    const { getByText } = render(<DisplayHeader scoreCount={-1} />);
+    const scoreValue = getByText("-1");
+
+    expect(scoreValue).toBeInTheDocument();
   });
 });
